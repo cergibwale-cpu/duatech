5df133c2b6588182631141759bf016acbb6086fb
cergibwale-cpu <cergibwale@gmail.com>
2026-02-19 07:10:27 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Admin from './pages/Admin'; 
+import App from './App.jsx';
+import './index.css';
+
+// यहाँ पर अब कोई अलग से Router या Admin इंपोर्ट करने की ज़रूरत नहीं है
+// क्योंकि वो सब आपकी App.jsx फाइल के अंदर पहले से सेट हैं।
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/pages/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
